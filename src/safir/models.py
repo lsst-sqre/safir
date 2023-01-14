8cf74ec7c6832574ef1024ca6c64a49219f08171
Russ Allbery <rra@lsst.org>
2023-01-13 17:09:58 -0800

    Add ErrorLocation enum to models
    Add an enum representing the valid first members of the loc array
    in the ErrorDetail model.  Due to the array (and backward
    compatibility) the typing can't be enforced, but it's still of use
    to applications that use Safir and construct custom errors.

--- src/safir/models.py
+++ src/safir/models.py
@@ -17,7 +17,11 @@ similar to this:
        raise HTTPException(
            status_code=status.HTTP_404_NOT_FOUND,
            detail=[
-               {"loc": ["path", "foo"], "msg": msg, "type": "invalid_foo"},
+               {
+                   "loc": [ErrorLocation.path, "foo"],
+                   "msg": msg,
+                   "type": "invalid_foo",
+               },
            ],
        )
 
@@ -31,11 +35,29 @@ generate good documentation.
 # Examples and notes kept in the module docstring because they're not
 # appropriate for the API documentation generated for a service.
 
+from enum import Enum
 from typing import List, Optional
 
 from pydantic import BaseModel, Field
 
-__all__ = ["ErrorDetail", "ErrorModel"]
+__all__ = [
+    "ErrorDetail",
+    "ErrorLocation",
+    "ErrorModel",
+]
+
+
+class ErrorLocation(str, Enum):
+    """Possible locations for an error.
+
+    The first element of ``loc`` in `ErrorDetail` should be chosen from one of
+    these values.
+    """
+
+    body = "body"
+    header = "header"
+    path = "path"
+    query = "query"
 
 
 class ErrorDetail(BaseModel):
