3596ae7d2cf66206cf419b59a3b616591ecc3fe2
Russ Allbery <rra@lsst.org>
2024-08-02 15:52:38 -0700

    Break safir.kubernetes up into internal modules
    Move the code for safir.kubernetes out of __init__.py into internal
    modules and turn __init__.py into only imports and re-exports.

---
+++ safir/src/safir/kubernetes/_init.py
@@ -0,0 +1,30 @@
+"""Initializing a Kubernetes client."""
+
+from __future__ import annotations
+
+import os
+
+from kubernetes_asyncio import config
+
+__all__ = ["initialize_kubernetes"]
+
+
+async def initialize_kubernetes() -> None:
+    """Load the Kubernetes configuration.
+
+    This has to be run once per process and should be run during application
+    startup.  This function handles Kubernetes configuration independent of
+    any given Kubernetes client so that clients can be created for each
+    request.
+
+    Notes
+    -----
+    If ``KUBERNETES_PORT`` is set in the environment, this will use
+    ``load_incluster_config`` to get configuration information from the local
+    pod metadata.  Otherwise, it will use ``load_kube_config`` to read
+    configuration from the user's home directory.
+    """
+    if "KUBERNETES_PORT" in os.environ:
+        config.load_incluster_config()
+    else:
+        await config.load_kube_config()
