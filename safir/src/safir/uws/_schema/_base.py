d218f5f7ac7b016116b14a3898400a2c9d2cd7f6
Russ Allbery <rra@lsst.org>
2024-09-10 12:04:25 -0700

    Reorganize UWS database schema
    Using a directory named `_schema` with private modules inside it
    does not work correctly with Sphinx and automodapi. Sphinx is unable
    to find the imported and re-exported symbols from those private
    modules.
    
    The separation of the schema into separate files is unnecessary, so
    merge them all together into a single `_schema.py` module file, which
    does work correctly with Sphinx.
    
    Convert to the new style of explicit inheritance from DeclarativeBase
    and use `Mapped`, `mapped_column`, and type derivation from type
    annotations instead of `Column`. This is the new, recommended way of
    doing ORM definitions in SQLAlchemy. Bump the version dependency for
    `safir[db]` to SQLAlchemy 2.0.0 or later since some of the elements of
    this new style were only added in the runup to the 2.0.0 release.

--- safir/src/safir/uws/_schema/_base.py
+++
@@ -1,9 +0,0 @@
-"""The base for the table schemas."""
-
-from __future__ import annotations
-
-from sqlalchemy.orm import declarative_base
-
-__all__ = ["UWSSchemaBase"]
-
-UWSSchemaBase = declarative_base()
