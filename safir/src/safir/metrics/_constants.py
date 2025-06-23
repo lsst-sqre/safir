f62656d4946da25a9bae1266dd7eb3a1536186f4
Dan Fuchs <danfuchs@slac.stanford.edu>
2025-06-23 17:01:04 -0500

    DM-48229: metrics - try to self-heal (if we initialized successfullly)
    If app metrics gets into an error state after successful initialization, the app may start sending metrics again by itself after the underlying infrastructure is fixed. After the first failure, it will try again after a configurable amount of time if another metric publish attempt happens.

--- safir/src/safir/metrics/_constants.py
+++ safir/src/safir/metrics/_constants.py
@@ -1,5 +1,7 @@
 """Constants for metrics functionality."""
 
+from datetime import timedelta
+
 BROKER_PREFIX = "safir-metrics-faststream-broker"
 """Prefix for the Kafka client id for the metrics FastStream broker."""
 
@@ -8,3 +10,6 @@ ADMIN_CLIENT_PREFIX = "safir-metrics-admin-client"
 
 EVENT_MANAGER_DEFAULT_KAFKA_TIMEOUT_MS = 1000
 """How long to wait for Kafka before raising an error for a blocking call."""
+
+EVENT_MANAGER_DEFAULT_BACKOFF_INTERVAL = timedelta(seconds=300)
+"""Amount of time to wait before taking event manager out of error mode."""
