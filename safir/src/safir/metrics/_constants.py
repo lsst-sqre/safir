89ee9cd38b532c041b482dc7e13746d1774e632b
Dan Fuchs <danfuchs@slac.stanford.edu>
2025-06-23 17:01:04 -0500

    DM-48229: metrics - Set a low kafka request timeout
    So that app metrics functionality doesn't block the main app for too
    long if the underlying kafka infrastructure goes down.

--- safir/src/safir/metrics/_constants.py
+++ safir/src/safir/metrics/_constants.py
@@ -5,3 +5,6 @@ BROKER_PREFIX = "safir-metrics-faststream-broker"
 
 ADMIN_CLIENT_PREFIX = "safir-metrics-admin-client"
 """Prefix for the client id for the metrics Kafka admin client."""
+
+EVENT_MANAGER_DEFAULT_KAFKA_TIMEOUT_MS = 1000
+"""How long to wait for Kafka before raising an error for a blocking call."""
